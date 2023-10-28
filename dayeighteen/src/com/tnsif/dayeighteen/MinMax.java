8f803173329801a642f9840fdba0b0ca37779fe5
yashaswiniraje <yashaswinirajebhosle@gmail.com>
2023-10-28 10:54:31 +0530

    commit

---
+++ dayeighteen/src/com/tnsif/dayeighteen/MinMax.java
@@ -0,0 +1,10 @@
+//Program to demonstrate generic interface
+package com.tnsif.dayeighteen;
+
+//Generic Interface
+public interface MinMax<T extends Comparable<T>> {
+
+	T getMinimum();
+
+	T getMaximum();
+}
